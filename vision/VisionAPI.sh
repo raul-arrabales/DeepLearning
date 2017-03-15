c399a2eee2dabeb5ff6c376bb560a38ae2f338fd
Raul Arrabales <raul.arrabales@gmail.com>
2017-03-15 21:46:10 +0100

    Create VisionAPI.sh

---
+++ vision/VisionAPI.sh
@@ -0,0 +1,5 @@
+# Install python client library
+pip install --upgrade google-cloud-vision 
+
+# Download Google Cloud SDK
+wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-147.0.0-linux-x86_64.tar.gz 
