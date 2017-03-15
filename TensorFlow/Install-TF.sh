bf47c4c44f9881c3de2cb29135b118d1a0bf391d
Raul Arrabales <raul.arrabales@gmail.com>
2017-03-15 10:37:28 +0100

    Update Install-TF.sh

--- TensorFlow/Install-TF.sh
+++ TensorFlow/Install-TF.sh
@@ -1,2 +1,14 @@
 # Install python pip and TF virtualenv
 sudo apt-get install python-pip python-dev python-virtualenv 
+
+# Create TF virtualenv in TF folder
+virtualenv --system-site-packages TF 
+
+# Activate the virtual env. 
+source ~/TF/bin/activate 
+# (use command deactivate to deactivate the env.)
+
+# Install TensorFlow in this virtualenv
+pip install --upgrade tensorflow 
+
+
