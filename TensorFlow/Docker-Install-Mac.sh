278edcfaf29779bc00f7e0d988f427fdfe347be0
Raul Arrabales <raul.arrabales@gmail.com>
2019-05-31 10:37:22 +0200

    Update Docker-Install-Mac.sh

--- TensorFlow/Docker-Install-Mac.sh
+++ TensorFlow/Docker-Install-Mac.sh
@@ -2,3 +2,6 @@
 # https://hub.docker.com/editions/community/docker-ce-desktop-mac 
 
 docker pull tensorflow/tensorflow 
+
+docker run -it -p 8888:8888 tensorflow/tensorflow  # Start a Jupyter notebook server
+
